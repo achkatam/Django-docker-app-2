a455eee302a0ab8f7d5a47f495b13b0bf675cc08
achkatam <agmatevv@gmail.com>
2023-12-13 07:48:37 -0500

    app created, model created

---
+++ secondTryApp/djnagoApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjnagoappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djnagoApp'
---
+++ secondTryApp/djnagoApp/views.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import User
+from serializers import UserSerializer
+# Create your views here. I need no views but I'll be using the file as controller file
+
